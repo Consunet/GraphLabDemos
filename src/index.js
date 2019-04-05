404d6cd184dc228ad98a5ab4cac5b2e3c5b091bb
Robert Wolf <robertwolf@consunet.com.au>
2019-04-05 12:29:57 +1030

    JSON-based configuration introduced.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ import GraphLab from '@consunet/graph-lab';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-var graph = new GraphLab.graph('GraphLabDiv');
+var graph = new GraphLab.graph('GraphLabDiv', {
+  scaling: 0.4,
+  yaw: -0.5,
+  roll: -0.5,
+  range_y: {from: -4, to: 4},
+  range_z: {from: Math.PI * -2, to: Math.PI * 2, tick: Math.PI * 0.125},
+  range_x: {from: Math.PI * -2, to: Math.PI * 2, tick: Math.PI * 0.125},
+  x_name: 'x', z_name: 'z', y_name: 'y'
+});
 
 var offset = 0;
 
@@ -18,14 +26,7 @@ graph.insertSwitchPanel();
 graph.createParameterBlock();
 graph.createParameter('Amplitude', 0, 1, 0.01, 1);
 
-graph.range_x(Math.PI * -2, Math.PI * 2, Math.PI * 0.125);
-graph.range_z(Math.PI * -2, Math.PI * 2, Math.PI * 0.125);
-graph.range_y(-4, 4);
-
-graph.scaling = 0.4;
-graph.reRoll(-0.5, -0.5);
-
-setInterval(function() {graph.reGraph(); offset += 0.04;}, 100);
+graph.animate(100, function() {offset += 0.04;});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
